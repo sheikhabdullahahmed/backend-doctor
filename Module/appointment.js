338937f344f9647596c3b4d964d1d0ef8f7077f5
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-11-10 17:49:40 +0500

    chnages

--- Module/appointment.js
+++ Module/appointment.js
@@ -1,15 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const appointmentSchema = new mongoose.Schema({
-//   patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
-//   doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
-//   date: String,
-//   time: String,
-//   status: { type: String, default: "active" }, // active, cancelled
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Appointment", appointmentSchema);
-
 const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema(
